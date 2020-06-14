2b3b8dd5195683202663bed5a703705397cffa8b
Shashwat verma <42150187+zippytyro@users.noreply.github.com>
2020-06-14 22:07:23 +0530

    update

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-let weight = document.getElementById("weight");
-let height = document.getElementById("height");
-let bmiResult = document.getElementById("bmiResult");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const bmiResult = document.getElementById("bmiResult");
 
 // event listener
 document.querySelector("button").addEventListener("click", function () {
     // calculation
     let bmi = weight.value / Math.pow(height.value, 2);
     bmi = Math.floor(bmi);
-    if (weight.value == 0 || height.value == 0) {
-        bmiResult.innerText = "Unvalid Input";
+    if (weight.value == 0 || height.value == 0 || weight.value = NaN || height.value = NaN) {
+        bmiResult.textContent = "Invalid Input. Try again.";
     } else {
-        bmiResult.innerText = "Your BMI is " + bmi;
+        bmiResult.textContent = "Your BMI is " + bmi;
     }
 });
